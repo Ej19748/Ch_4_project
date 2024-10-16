34ac6c5f5215c6c5483a3f6b0d459f4ab64eb349
Ej19748 <ej19748@email.vccs.edu>
2024-10-16 00:49:10 +0000

    Code

--- .ccls-cache/@home@runner@Chapter-4-project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-4-project/main.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 using namespace std;
-int main() 
+int main() {
+  int year, month;
+  cout << "Enter a year: ";
+  cin >> year;
+  cout << "Enter a month: ";
+  cin >> month;
+  cout << endl;
+  switch (month)
+    {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        cout << "31 days";
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        cout << "30 days";
+        break;
+      case 2:
+        if (year % 4 == 0)
+          cout << "29 days";
+        else
+          cout << "28 days";
+        break;
+      default:
+        cout << "Invalid month";
+    
+    }
+  return 0;
+}
